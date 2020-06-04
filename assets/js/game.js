0810bca54faa76f6ec2d7b12ac92603edb703314
JessBot500 <jessica.bergstrom20@outlook.com>
2020-06-03 21:27:26 -0500

    loop through arrays

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,7 +7,7 @@ var playerAttack = 10;
 // you can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+var enemyNames = [" Roborto ", " Amy Android ", " Robo Trumble "];
 console.log(enemyNames[0]);
 console.log(enemyNames[1]);
 console.log(enemyNames[2]);
@@ -16,6 +16,11 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 console.log(enemyNames.length);
+for (var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
 
 var fight = function () {
     // Alert users that they are starting the round
